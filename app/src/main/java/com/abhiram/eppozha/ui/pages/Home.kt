e8dee934556bbfc948fb84c7da3f7acd36b89e3c
Abhiram <anandabhiram0.ar@gmail.com>
2024-01-20 13:22:59 +0530

    feat TextFields

--- app/src/main/java/com/abhiram/eppozha/ui/pages/Home.kt
+++ app/src/main/java/com/abhiram/eppozha/ui/pages/Home.kt
@@ -1,5 +1,6 @@
 package com.abhiram.eppozha.ui.pages
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -12,24 +13,45 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
+import com.abhiram.eppozha.ui.theme.ButtonBackground
 import com.abhiram.eppozha.ui.theme.CardBorder
 import com.abhiram.eppozha.ui.theme.Grey
+import com.abhiram.eppozha.ui.theme.LightGrey
 import com.abhiram.eppozha.ui.theme.OffWhite
 import com.abhiram.eppozha.ui.theme.Purple40
+import com.abhiram.eppozha.viewmodels.ApiViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun Home(){
+    val viewModel : ApiViewModel = ApiViewModel()
+    var context = LocalContext.current
     Box(modifier = Modifier.fillMaxSize(1F)){
         Box(
             modifier = Modifier
@@ -40,7 +62,7 @@ fun Home(){
         )
         Box(
             modifier = Modifier
-                .fillMaxHeight(0.7F)
+                .fillMaxHeight(0.6F)
                 .fillMaxWidth(1F)
                 .shadow(
                     elevation = 10.dp,
@@ -48,7 +70,7 @@ fun Home(){
                 )
                 .clip(RoundedCornerShape(bottomEnd = 20.dp, bottomStart = 20.dp))
                 .background(Grey)
-                .padding(start = 24.dp, top = 20.dp)
+                .padding(start = 24.dp, top = 32.dp)
                 .align(Alignment.TopCenter)
         ){
             Column{
@@ -74,8 +96,7 @@ fun Home(){
 
         Box(
             modifier = Modifier
-                .padding(top = 82.dp)
-                .fillMaxHeight(0.69F)
+                .padding(top = 64.dp)
                 .fillMaxWidth(0.9F)
                 .shadow(
                     elevation = 10.dp,
@@ -87,6 +108,68 @@ fun Home(){
                 .align(Alignment.Center)
 
         ){
+            Column(
+                modifier = Modifier
+                    .padding(start = 24.dp, top = 46.dp, end = 24.dp, bottom = 46.dp)
+                    .fillMaxWidth(1F)
+            ) {
+                Text(
+                    text = "Where are you going?",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Grey
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                var from by remember {
+                    mutableStateOf(TextFieldValue(""))
+                }
+                OutlinedTextField(
+                    value = from,
+                    onValueChange = { from = it},
+                    modifier = Modifier.fillMaxWidth(1F),
+                    label = {
+                        Text(
+                            text = "From",
+                            color = Grey
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                var to by remember {
+                    mutableStateOf(TextFieldValue(""))
+                }
+                OutlinedTextField(
+                    value = to,
+                    onValueChange = { to = it},
+                    modifier = Modifier.fillMaxWidth(1F),
+                    label = {
+                        Text(
+                            text = "To",
+                            color = Grey
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = {
+                        if (from.text.isNotEmpty()&&to.text.isNotEmpty()){
+                            viewModel.viewModelScope.launch {
+                                var res = viewModel.GetSchedule(from.text,to.text,null,null)
+                                Toast.makeText(context, res, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(1F),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = ButtonBackground, contentColor = OffWhite)
+                ) {
+                    Text(
+                        text = "Get Results",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
 
         }
     }
--- app/src/main/java/com/abhiram/eppozha/viewmodels/ApiViewModel.kt
+++ app/src/main/java/com/abhiram/eppozha/viewmodels/ApiViewModel.kt
@@ -1,28 +1,31 @@
 package com.abhiram.eppozha.viewmodels
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.abhiram.eppozha.repositories.datamodels.ApiResponse
 import com.abhiram.eppozha.repositories.retrofit.BusApi
 import com.abhiram.eppozha.repositories.retrofit.RetrofitHelper
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class ApiViewModel : ViewModel() {
-    fun GetSchedule(
+    suspend fun GetSchedule(
         departure : String,
         destination  : String,
         time : String?,
         restrict : Boolean?
-    ){
+    ): String {
         val api = RetrofitHelper.getInstance().create(BusApi::class.java)
+        var res: Response<ApiResponse>? = null
         val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
             throwable.printStackTrace()
         }
-        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler){
-            val res = api.GetSchedule(departure,departure,time,restrict)
-            Log.e("Response",res.toString())
-        }
+        res = api.GetSchedule(departure,departure,time,restrict)
+        Log.e("Response",res.toString())
+        return res.toString()
     }
 }
